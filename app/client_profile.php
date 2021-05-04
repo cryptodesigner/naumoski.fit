1d1b85da627a31541e533c9a7b8e8ae021c7c74e
CryptoDesign <49728568+cryptodesigner@users.noreply.github.com>
2021-05-04 11:27:38 +0200

    add chat to manager and client profile

--- app/client_profile.php
+++ app/client_profile.php
@@ -7,3 +7,27 @@
   $childView = 'views/_client_profile.php';
   include('layout_client.php');
 ?>
+<!-- Facebook Messenger -->
+<div class="fb-customerchat"
+ page_id="105973318241762"
+ greeting_dialog_display="hide"
+ theme_color="#A38800">
+</div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '912333495590130',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+  };
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+<!-- End Facebook Messenger -->
