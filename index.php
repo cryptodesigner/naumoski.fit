ef7d3b2904e1bc4598649f147160a12dc17422ff
Orhan Kurto <orhankurto1@gmail.com>
2021-05-11 09:47:28 +0200

    created email form

--- index.php
+++ index.php
@@ -859,40 +859,35 @@
             </div>
 
             <div class="col-12 col-lg-6 contact-form-box">
-                <form class="contact-form" id="contact-form-data">
+                <form method="POST" class="contact-form" id="contact-form-data">
                     <div class="row">
-                        <!-- Submission Popup -->
-                        <div class="col-12">
-                            <div class="col-sm-12 px-0" id="result"></div>
-                        </div>
-
                         <div class="col-12 col-md-6">
                             <div class="form-group">
-                                <input class="form-control" type="text" placeholder="Full Name:" required="" id="first_name" name="firstName">
+                                <input class="form-control" type="text" placeholder="Full Name:" required="" id="name" name="name">
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <!-- <div class="col-12 col-md-6">
                             <div class="form-group">
                                 <input class="form-control" type="text" placeholder="Country:" required="" id="last_name" name="lastName">
                             </div>
-                        </div>
+                        </div> -->
                         <div class="col-12 col-md-6">
                             <div class="form-group">
-                                <input class="form-control" type="email" placeholder="Email:" required="" id="email" name="userEmail">
+                                <input class="form-control" type="text" placeholder="Email:" required="" id="email" name="email">
                             </div>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <!-- <div class="col-12 col-md-6">
                             <div class="form-group">
                                 <input class="form-control" type="tel" placeholder="Phone:" id="phone" name="userPhone">
                             </div>
-                        </div>
+                        </div> -->
                         <div class="col-12">
                             <div class="form-group">
-                                <textarea class="form-control" placeholder="Message" id="message" name="userMessage"></textarea>
+                                <textarea class="form-control" placeholder="Message" id="message" name="message"></textarea>
                             </div>
                         </div>
                         <div class="col-12">
-                            <button type="button" class="btn btn-medium green-long-btn rounded-pill w-100 btn-pink mt-4 contact_btn" id="submit_btn">SUBMIT REQUEST</button>
+                            <button type="submit" value="Send" class="btn btn-medium green-long-btn rounded-pill w-100 btn-pink mt-4">SUBMIT</button>
                         </div>
                     </div>
                 </form>
@@ -999,3 +994,60 @@
 <!-- End Facebook Messenger -->
 </body>
 </html>
+
+<?php
+/* Email input from a live contact form to your desired email address through a SMTP and access to your Gmail account of choice. */
+
+$msg = '';
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+
+    require_once("php/PHPMailerAutoload.php");
+    
+    require("php/PHPMailer.php");
+    require("php/SMTP.php");
+    require("php/Exception.php");
+    
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    //Tell PHPMailer to use SMTP - requires a local mail server so don't use
+    //$mail->isSMTP();
+
+    // 0 = off // 1 = client messages // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = 'html';
+    $mail->Host = 'localhost';
+    $mail->Port = 25;
+    //$mail->SMTPSecure = 'tls';
+    //$mail->SMTPAuth = true;
+    $mail->Username = "contact@naumoski.fit";
+    $mail->Password = "Naumoski@2021";
+
+    // Set as the same email address you just gave up the password to up above.
+    $mail->setFrom('contact@naumoski.fit', 'First Last');
+    // Where do you want the message to be sent?
+    $mail->addAddress('contact@naumoski.fit', 'Name Here');
+
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        // Edit the Subject line below, as desired
+        $mail->Subject = 'NaumoskiFit';
+        $mail->isHTML(false);
+        // If your form has other fields, add them below. ie: Phone: {$_POST['phone']}
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+          // JS popup alert to let user know the message was sent.
+          $message = 'Message sent! Thanks for contacting us.';
+          echo "<script type='text/javascript'>alert('$message');</script>";
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
+?>
